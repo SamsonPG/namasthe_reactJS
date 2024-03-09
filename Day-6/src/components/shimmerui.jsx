894fe4fe0626e7c8a5cb7094c50e41c86a84b1a5
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-03-09 20:41:54 +0530

    added index for shimmerui

--- Day-6/src/components/shimmerui.jsx
+++ Day-6/src/components/shimmerui.jsx
@@ -1,16 +1,16 @@
 const Shimmer = ()=>{
     return(
         <>
-        {Array(8).fill("").map((e=>
-            <div className="shimmer-card">
+        {Array(8).fill("").map((e, index)=>(
+            <div key={index} className="shimmer-card">
             <div className="shimmer-image-container"></div>
             <br/>
             <h3 className="shimmer-text"></h3>
             <h4 className="shimmer-text"></h4>
             <h6 className="shimmer-text"></h6>
             <h5 className="shimmer-text"></h5>
-            </div>
-        ))}
+            </div>)
+        )}
           </>
     )
   
