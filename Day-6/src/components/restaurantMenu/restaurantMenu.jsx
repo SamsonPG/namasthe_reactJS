dbff620e2b8cce7b518c5981ae30649eb47c7b8c
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-03-16 23:11:48 +0530

    restaurant menu

--- Day-6/src/components/restaurantMenu/restaurantMenu.jsx
+++ Day-6/src/components/restaurantMenu/restaurantMenu.jsx
@@ -1,46 +1,53 @@
-import {useParams} from "react-router-dom";
-import {useState, useEffect} from "react";
-import {cloudinaryLink} from "../../constant"
-const RestaurantMenu =()=>{
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { cloudinaryLink } from "../../constant";
 
-    const {resID} = useParams();
-   
-    const [restaurantName,setRestaurantName] = useState({});
-    const [restaurantMenu,setRestaurantMenu] = useState({});
+const RestaurantMenu = () => {
+    const { resID } = useParams();
+    const [restaurantName, setRestaurantName] = useState({});
+    const [restaurantMenu, setRestaurantMenu] = useState([]);
 
-    useEffect(()=>{
-        getRestaurentMenu()
-    },[])
-
-
-    async function getRestaurentMenu(){
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=10.5276416&lng=76.2144349&restaurantId=265398&catalog_qa=undefined&submitAction=ENTER")
-        const json= await data.json();
-        setRestaurantName(json?.data?.cards[0]?.card?.card?.info)
-        setRestaurantMenu(json?.data?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.categories[0]?.itemCards[0]?.card?.info)
-       
-    }
     useEffect(() => {
-        console.log(restaurantMenu);
-    }, [restaurantMenu]); // Runs whenever restaurantMenu changes
+        getRestaurantMenu();
+    }, []);
 
- 
-    return(
+    async function getRestaurantMenu() {
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=10.5276416&lng=76.2144349&restaurantId=73241");
+        const json = await data.json();
+        setRestaurantName(json?.data?.cards[0]?.card?.card?.info);
+        const categories = json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.categories;
+        if (categories) {
+            const menuItems = categories.flatMap(category => category.itemCards);
+            setRestaurantMenu(menuItems);
+        }
+        else{
+            setRestaurantMenu(json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards)
+        }
+    }
+
+    return (
         <div>
-        <div>
-            <h1>Restaurant id : {resID}</h1>
-            <h2>{restaurantName.name}</h2>
-            <img src={cloudinaryLink + restaurantName.cloudinaryImageId}/>
-            <h3>{restaurantName.locality}</h3>
-            <h3>{restaurantName.areaName}</h3>
-            <h3>{restaurantName.costForTwoMessage}</h3>
-            <h3>{restaurantName.avgRating} Stars</h3>
+            <div>
+                <h1>Restaurant id : {resID}</h1>
+                <h2>{restaurantName.name}</h2>
+                <img src={cloudinaryLink + restaurantName.cloudinaryImageId} alt={restaurantName.name} />
+                <h3>{restaurantName.locality}</h3>
+                <h3>{restaurantName.areaName}</h3>
+                <h3>{restaurantName.costForTwoMessage}</h3>
+                <h3>{restaurantName.avgRating} Stars</h3>
+            </div>
+            <div>
+                {restaurantMenu.map(item => (
+                    <div key={item.card.info.id}>
+                        <h4>{item.card.info.name}</h4>
+                        <p>Category: {item.card.info.category}</p>
+                        <p>Price: {item.card.info.price}</p>
+                        <p>Ratings: {item.card.info.ratings.aggregatedRating?.rating}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-        <div>
-          
-        </div>
-        </div>
-    )
+    );
 }
 
 export default RestaurantMenu;
